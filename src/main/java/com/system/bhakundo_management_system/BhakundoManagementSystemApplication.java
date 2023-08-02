053af87abac9a66fc99dff148194cddceb2c6e4c
shahrishav <shahrishav11@gmail.com>
2023-08-02 15:47:33 +0545

    first commit

---
+++ src/main/java/com/system/bhakundo_management_system/BhakundoManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.system.bhakundo_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhakundoManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhakundoManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/bhakundo_management_system/BhakundoManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.bhakundo_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhakundoManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
