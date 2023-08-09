bd913883af526f093919ec277cc90a3c7fc7108c
shahrishav <shahrishav11@gmail.com>
2023-08-10 00:45:23 +0545

    Pojo 3

---
+++ src/main/java/com/system/bhakundo_management_system/Pojo/UserPojo.java
@@ -0,0 +1,36 @@
+package com.system.bhakundo_management_system.Pojo;
+
+
+import com.system.bhakundo_management_system.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class UserPojo {
+    private Integer id;
+    private String email;
+    private  String mobile_no;
+    private String password;
+    private String name;
+    private String address;
+
+
+
+    public UserPojo(User user) {
+        this.id = user.getId();
+        this.email = user.getEmail();
+        this.mobile_no = user.getMobileNo();
+        this.name = user.getName();
+        this.password = user.getPassword();
+        this.address = user.getAddress();
+
+    }
+}
+
+
