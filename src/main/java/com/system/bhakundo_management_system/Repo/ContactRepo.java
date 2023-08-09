479f863496ceab13815f45e5ec50b01abfac1678
shahrishav <shahrishav11@gmail.com>
2023-08-10 00:48:29 +0545

    Repo 3

---
+++ src/main/java/com/system/bhakundo_management_system/Repo/ContactRepo.java
@@ -0,0 +1,10 @@
+package com.system.bhakundo_management_system.Repo;
+
+import com.system.bhakundo_management_system.entity.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepo extends JpaRepository<Contact ,Integer> {
+
+}
+
+
