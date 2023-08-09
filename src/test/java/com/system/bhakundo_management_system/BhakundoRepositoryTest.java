7e13685379b81d46fda10b32a5ff27820a00e858
shahrishav <shahrishav11@gmail.com>
2023-08-10 01:36:10 +0545

    testing

---
+++ src/test/java/com/system/bhakundo_management_system/BhakundoRepositoryTest.java
@@ -0,0 +1,58 @@
+package com.system.bhakundo_management_system;
+
+import com.system.bhakundo_management_system.Repo.BhakundoRepo;
+import com.system.bhakundo_management_system.entity.Bhakundo;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.core.annotation.Order;
+
+@DataJpaTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class BhakundoRepositoryTest {
+    @Autowired
+    private BhakundoRepo bhakundoRepo;
+
+
+    @Test
+    @Order(1)
+    public void savebhakundoTest() {
+
+        Bhakundo bhakundo = Bhakundo.builder()
+                .bhakundoname("rak")
+                .bhakundocontact("123454")
+                .bhakundoprice("98888888")
+                .build();
+
+
+        bhakundoRepo.save(bhakundo);
+
+        Assertions.assertThat(bhakundo.getBasket_ballId()).isGreaterThan(0);
+    }
+
+    @Test
+    @Order(4)
+    public void updatebhakundoTest(){
+
+        Bhakundo bhakundo = Bhakundo.builder()
+                .bhakundoname("rak")
+                .bhakundocontact("123454")
+                .bhakundoprice("98888888")
+                .build();
+
+
+        bhakundoRepo.save(bhakundo);
+
+        Bhakundo bhakundo1 = bhakundoRepo.findById(bhakundo.getBasket_ballId()).get();
+
+        bhakundo1.setBhakundocontact("13265");
+
+        Bhakundo bhakundoupdated  = bhakundoRepo.save(bhakundo);
+
+        Assertions.assertThat(bhakundoupdated.getBhakundocontact()).isEqualTo("85207410");
+
+    }
+}
